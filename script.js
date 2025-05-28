ba324da8f1c04bd67f5690798f0e51641538f0c5
Asim14878111 <asim14875111@gmail.com>
2025-05-29 02:37:01 +0500

    New Task

---
+++ script.js
@@ -0,0 +1,48 @@
+let count = 250
+counter = 1
+function add(id1,id2){
+    count = count++
+    counter++
+    const counter2 = document.getElementById(id1).innerHTML = counter
+
+    const price = document.getElementById(id2).textContent = Math.trunc(count  +=  250);
+}
+
+function minus(id1,id2){
+    
+if(counter > 1){
+counter--
+count = count--
+const counter2 = document.getElementById(id1).innerHTML = counter
+
+const price = document.getElementById(id2).textContent = Math.trunc(count  -=  250);
+}
+
+
+}
+
+function chngcontent(Id){
+    const container2 = document.getElementById(Id)
+    const container = document.getElementById('container')
+    container.classList.add('none-display')
+    // const container2 = document.getElementById('2ndcontainer')
+container2.classList.add('block-display')
+createSlug(str)
+}
+
+
+function displaymainpg(Id){
+    const container2 = document.getElementById(Id)
+    container2.classList.remove('block-display')
+const container = document.getElementById('container')
+container.classList.remove('none-display')
+
+}
+
+
+count2 = 1
+function addtocart(Id){
+    const button = document.getElementById(Id)
+button.innerHTML = count2++
+
+}
