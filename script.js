fcc055b0e3378912f15f516e359431d5ae3c0eb1
Asim14878111 <asim14875111@gmail.com>
2025-05-31 02:27:15 +0500

    New Task

--- script.js
+++ script.js
@@ -14,7 +14,6 @@ function add(id1,id2){
 }
 
 function minus(id1,id2){
-    
 if(counter > 1){
 counter--
 count = count--
@@ -55,7 +54,7 @@ chng2ndurl.addEventListener('click', function(){
     const newurl = `${newslug}/nike-revolution`
     window.history.replaceState({path: newurl}, '', newurl)
 })
-// /
+
 
 // 3rd product url
 
@@ -120,23 +119,4 @@ button.innerHTML = count2++
 }
 
 
-
-// const Productslug = "Shoes-store"
-// window.history.pushState({},"",`/${Productslug}`)
-// console.log("Current URL:", window.location.href);
-
-
-// const addtocartbtn = document.getElementById('main-img-button')
-// addtocartbtn.addEventListener('click', () =>{
-// })
-
-// const productSlug = "Shoes-store";
-// // Update the URL without reloading the page
-// window.history.pushState({}, "", `${productSlug}`);
-// console.log("Current URL:", window.location.href);
-
-// const base ='http://127.0.0.1:5500/'
-// const url = new URL('shoes/store/pk',base)
-// console.log(url)
-// window.location.href = url
 
